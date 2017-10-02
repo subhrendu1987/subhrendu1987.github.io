9fa58b685c4d137b87210f8d22c664f650460a6e
subhrendu-subho <subhrendu.subho@gmail.com>
2017-10-02 23:34:13 +0530

    modified:   ../README.md 	new file:   ConferenceCrawler.py 	deleted:    ConferenceRanking.xlsx(1) 	new file:   Conflist_Autognerated.html 	new file:   TemplateBottom.html 	new file:   TemplateTop.html 	new file:   myLibrary.py

---
+++ conflist/ConferenceCrawler.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+'''
+Crawler for Conference ranking
+'''
+import datetime,subprocess,sys,os,json
+import time,hashlib
+import networkx as nx
+import matplotlib.pyplot as plt
+from random import randint
+import random, re, commands
+from time import sleep
+from copy import copy
+from datetime import datetime
+from myLibrary import *
+############################################################################
+def GetHTMLTemplate(filename):
+	data = [line.strip() for line in open(filename, 'r')]
+	return(data)
+############################################################################
+def TruncateDict(D,items=None):
+	if None:
+		return(D)
+	KEYS=D.keys()[0:items]
+	ret={k:D[k] for k in KEYS}
+	return(ret)
+############################################################################
+def generateHTML(message):
+	m="\n".join(message)
+	f = open('Conflist_Autognerated.html','w')
+	f.write(m)
+	f.close()
+############################################################################
+ConfList=getDictFrom("ConferenceRanking_All.json")
+temp=TruncateDict(ConfList,5)
+Confs= [GetConferenceRanking(acronym) for acronym in temp]#Parse Core WebPage
+####################################
+SortedConfs = sorted(Confs, key=lambda k: k['Rank']) 
+body1=GetHTMLTemplate("TemplateTop.html")
+body3=GetHTMLTemplate("TemplateBottom.html")
+body2=[]
+Ranks=set([item['Rank'] for item in SortedConfs])
+####################################
+NAVBAR=[]
+for r in Ranks:
+	NAVBAR=NAVBAR+["<td class=\"Change Cicon\" style=\"width:75px\">",
+		"<a id=\"NavBarLink\" class=\"NavLink\" href=\"#%s\">%s</a>"%(r.encode('ascii'), r.encode('ascii')),
+		"</td>"]
+body2=body2+["<table><tr align=\"center\">"]+NAVBAR+["</tr></table>"]	
+body2=body2+["<!------------------------------------------------------------------------------------->"]
+####################################
+ConfTable=["<tr><th>Rank</th><th>Acronym</th><th>Title</th></tr>"]
+for c in SortedConfs:
+	ConfTable=ConfTable+["<tr>",
+	"<td>%s</td>"%(c["Rank"].encode("ascii")),
+	"<td><button class=\"gsearch\" onClick=\"googleSearch()\">%s</button></td>"%(c["Acronym"].encode("ascii")),
+	"<td>%s</td></tr>"%(c["Title"].encode("ascii"))]
+body2=body2+["<table border=\"1\"  width=\"70\%\">"]+ConfTable+["</table>"]
+####################################
+''' SAMPLE
+<table border="1"  width="70%">
+<tr><td><button class="gsearch" onClick="googleSearch()">	USITS	</button></td><td>	USENIX Conf on Internet Tech and Sys	</td></tr>
+</table>
+</center>
+'''
+
+body2=body2+["<h6>Last Updated:%s</h6>"%(getTimeStamp())]
+message=body1+body2+body3
+generateHTML(message)
---
+++ conflist/myLibrary.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+import datetime,subprocess,sys,os,json
+import time,hashlib
+from random import randint
+import random, re, commands
+from time import sleep
+from copy import copy
+from bs4 import BeautifulSoup
+import requests
+############################################################################
+folders="/home/pi/flipperRPi3/"
+log_file=folders+"LOG/log.txt"
+############################################################################
+def hash_dict(d):
+    return hashlib.sha1(json.dumps(d, sort_keys=True)).hexdigest()
+############################################################################
+def getTimeStamp():
+	tstamp = str(datetime.datetime.now())
+	return(tstamp)
+############################################################################
+def printToLog(filename,logtext):
+	file_handler=open(filename,"a")
+	tstamp = getTimeStamp()
+	log_string="%s\t %s\n"%(tstamp,logtext)
+	file_handler.write(log_string)
+	file_handler.close()
+	return
+############################################################################
+def add_to_log(log):
+	file_handler=open(log_file,"a")
+	tstamp = getTimeStamp()
+	log_string="%s\t %s\n"%(tstamp,log)
+	file_handler.write(log_string)
+	file_handler.close()
+	return log_string
+############################################################################
+def getDictFrom(filename):
+	file_handler=open(filename,"r")
+	variable=json.load(file_handler)
+	file_handler.close()
+	return(variable)
+############################################################################
+def putDictTo(filename,variable):
+	file_handler=open(filename,"w")
+	json.dump(variable, file_handler)
+	file_handler.flush()
+	file_handler.close()
+	return
+############################################################################
+def GetConferenceRanking(acronym):
+	''' Parse Core'''
+	URL="http://portal.core.edu.au/conf-ranks/?search=%s&by=all&source=all&sort=atitle&page=1"%(acronym)
+	r  = requests.get(URL)
+	data = r.text
+	LeftPos=data.find("of",data.find("Showing results"))+len("of")
+	RightPos=data.find("\n",LeftPos)
+	Results=int(data[LeftPos:RightPos])
+	if Results <>1:
+		print  "ERROR: "+acronym+" Multiple Results"
+	DataSoup = BeautifulSoup(data)
+	table=DataSoup.find_all("table")
+	SoupCols=BeautifulSoup(str(table[0]))
+	cols=SoupCols.find_all("td")
+	items=[re.sub("\n","",c.get_text()).strip()  for c in cols]
+	headers=SoupCols.findChildren("b")
+	Names=[h.get_text() for h in headers]
+	if (len(Names) <> len(items)):
+		print "ERROR: Table Parsing Problem. "+acronym
+	TabDict={j:items[i] for i,j in enumerate(Names)}
+	return(TabDict)
+############################################################################
+def GetConferenceCFP(acronym):
+	''' Parse wikiCFP'''
+	URL="http://www.wikicfp.com/cfp/servlet/tool.search?q=%s&year=f"%(acronym)
+	r  = requests.get(URL)
+	data = r.text
+	discardPos=data.find("Matched Call For Papers for")
+	left=data[discardPos:]
+	DataSoup = BeautifulSoup(left)
+	table=str(DataSoup.find_all("table")[0]) # need to handle blank results
+	SoupRows=BeautifulSoup(table)
+	rows=SoupRows.find_all("tr")
+	SoupCols=BeautifulSoup(str(rows[0]))
+	headers=SoupCols.findChildren("td")
+	headers=SoupCols.findChildren("b")
+
+
+############################################################################
+def PullRemoteFile(ip,filename):
+		CMD="sshpass -p \"raspberry\" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no pi@%s:%s /tmp/tmp.json" %(ip,filename)
+		#CMD="sshpass -p \"raspberry\" scp pi@%s:%s /tmp/tmp.json"%(ip,filename)
+		proc = subprocess.Popen(CMD,shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+		(out, err) = proc.communicate()
+		if len(err) ==0:
+			variable = getDictFrom("/tmp/tmp.json")
+		else:
+			print "SSH Error! "+err
+			variable=None
+		return(variable)
+############################################################################
+def PullRemoteFileWget(ip,filename,port=8080):
+		tmp_file="/tmp/tmp.json"
+		CMD="wget http://%s:%d/%s -O %s"%(ip,port,"LOG/"+os.path.basename(filename),tmp_file)
+		proc = subprocess.Popen(CMD,shell=True, stdout=subprocess.PIPE)
+		(out, err) = proc.communicate()
+		#ret=os.system(CMD)
+		if(ret==0):
+			variable = getDictFrom(tmp_file)
+			os.system("rm %s"%(tmp_file))
+			return(variable)
+		else:
+			return(None)
+############################################################################
