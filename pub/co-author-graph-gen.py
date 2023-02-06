a7fc30a821e2deaa3ab2081005702b80329775b8
subhrendu1987 <subhrendu@idrbt.ac.in>
2023-02-06 16:57:50 +0530

    new file:   ChartData-1.html 	new file:   ChartData-2.html 	new file:   ChartData-3.html 	new file:   ChartData-4.html 	new file:   ChartData.html 	modified:   co-author-graph-gen.py 	new file:   weighted.coauthlist.tmp

--- pub/co-author-graph-gen.py
+++ pub/co-author-graph-gen.py
@@ -2,25 +2,27 @@ import bibtexparser
 import json
 import urllib
 import urllib.parse
-from chord import Chord
+#from chord import Chord
 from itertools import combinations
 import pandas as pd
 import networkx as nx
 import xmltodict
 import os
 import numpy as np
-import holoviews as hv
-from holoviews import opts, dim
-import holoviews.plotting.bokeh
+#import holoviews as hv
+#from holoviews import opts, dim
+#import holoviews.plotting.bokeh
 DBLP_URL="https://dblp.org/pid/141/2034.xml"
-BIB_FILE="0Subho_Resume/mypub.bib"
+BIB_FILE="common/20_bibilography/mypub.bib"
 PI=np.pi
 #############################################################
-def check_data(df):
-    L,M=data_matrix.shape
-    if L!=M:
-        raise ValueError('Data array must have (n,n) shape')
-    return L
+def createLegend(G):
+    legendList={}
+    reverseLegend={}
+    for i,name in enumerate(list(G.nodes())):
+    	legendList[name]=i+1
+    	reverseLegend[i+1]=name
+    return ((legendList,reverseLegend))
 #############################################################
 def fetchFromDBLP():
     file = urllib.request.urlopen(DBLP_URL)
@@ -36,51 +38,24 @@ def fetchFromBib():
 	bibList=bib_database.entries
 	return(bibList)
 #############################################################
-def get_ideogram_ends(ideogram_len, gap):
-    ideo_ends=[]
-    left=0
-    for k in range(len(ideogram_len)):
-        right=left+ideogram_len[k]
-        ideo_ends.append([left, right])   
-        left=right+gap
-    return ideo_ends 
-#############################################################
-def moduloAB(x, a, b): #maps a real number onto the unit circle identified with 
-                       #the interval [a,b), b-a=2*PI
-        if a>=b:
-            raise ValueError('Incorrect interval ends')
-        y=(x-a)%(b-a)
-        return y+b if y<0 else y+a
-#############################################################
-def test_2PI(x):
-    return 0<= x <2*PI
-#############################################################
-def drawChordDiagram(df,outFile="chordDiagram.html"):
-	check_data(df)
-	df.drop('Chattopadhyay, Subhrendu', axis=0,inplace=True)
-	df.drop('Chattopadhyay, Subhrendu', axis=1,inplace=True)
-	names = list(df.columns.values)
-	data = df.values.tolist()
-	df.to_csv('out.csv')  
-	matrix=df.to_numpy()
-
-	row_sum=[np.sum(matrix[k,:]) for k in range(len(matrix))]
-	#set the gap between two consecutive ideograms
-	gap=2*PI*0.005
-	ideogram_length=2*PI*np.asarray(row_sum)/sum(row_sum)-gap*np.ones(df.shape[0])
-	ideo_ends=get_ideogram_ends(ideogram_length, gap)
-	ideo_ends
-
-	return
-#############################################################
-def bibListToNetx(bibList)
+def bibListToNetx(bibList):
 	coauthCount={}
 	G=nx.multigraph.Graph()
-	bibList=fetchFromBib()
+	if(len(bibList)==0):
+		bibList=fetchFromBib()
 	for pub in bibList:
 		title=pub['title']
-		authors=pub['author'].split(" and ")
-		coauthors=list(combinations(authors, 2))
+		authors=pub['author'].split(" and ")  # Last Name First Format
+		## Process Authors
+		authors_formatted=[]
+		for auth in authors:
+			temp=auth.split(",")
+			if(len(temp)==2):
+				authors_formatted.append((temp[1]+" "+temp[0]).strip())
+			else:
+				print("3 Part Author name: Check entry: "+title)
+		coauthors=list(combinations(authors_formatted, 2))
+		#coauthors=list(combinations(authors, 2))
 		for auth2 in coauthors:
 			if G.has_edge(auth2[0],auth2[1]):
 				G.add_edge(auth2[0],auth2[1],weight=G[auth2[0]][auth2[1]]['weight']+1)
@@ -88,28 +63,50 @@ def bibListToNetx(bibList)
 				G.add_edge(auth2[0],auth2[1],weight=1)
 	return(G)
 #############################################################
-G=bibListToNetx(bibList)
-df=nx.to_pandas_adjacency(G)
+def graphToHTML(G,chart_gen_code_body_file):
+	#HTMLs=[]
+	with open("weighted.coauthlist.tmp") as file:
+		lines=file.readlines()
+	LNs=[l.strip().split(",") for l in lines]
+	
+	with open(chart_gen_code_body_file, "w+") as myfile:
+		for l in LNs:
+		  	htmlSyntax="{ source: \""+str(legendList[l[0].strip()])+"\", target: \""+str(legendList[l[1].strip()])+"\", value: "+str(l[2].strip())+"}"
+		  	#HTMLs.append(htmlSyntax)
+		  	myfile.write(htmlSyntax+",\n")
+		myfile.write("]); // Make stuff animate on load\n")
+		myfile.write(" series.appear(500, 100);\n")
+		myfile.write("}); // end\n")
+		myfile.write("\n am5.ready()\n")
+		myfile.write("</script>\n")
 
+	return
 #############################################################
-import pandas as pd
-from bokeh.plotting import output_file, Chord
-from bokeh.io import show
-from bokeh.sampledata.les_mis import data
+def legendToHTML(reverseLegend,chart_gen_code_legend_file):
+	#HTMLs=[]
+	with open(chart_gen_code_legend_file, "w+") as myfile:
+		myfile.write("<div id=\"chartdiv\"></div>\n")
+		myfile.write("<h2>Legends</h2>\n")
+		myfile.write("<ol>\n")
+		for k in reverseLegend.keys():
+			htmlSyntax="<li> "+str(reverseLegend[k])+"</li>"
+			#HTMLs.append(htmlSyntax)
+			myfile.write(htmlSyntax+"\n")
+		myfile.write("</ol>")
+	return
+#############################################################
+G=bibListToNetx([])
+legendList,reverseLegend =createLegend(G)
+nx.write_weighted_edgelist(G, "weighted.coauthlist.tmp", delimiter=",")
+chart_gen_code_body_file="ChartData-2.html"
+graphToHTML(G,chart_gen_code_body_file)
+
+chart_gen_code_legend_file="ChartData-3.html"
+legendToHTML(reverseLegend,chart_gen_code_legend_file)
 
 
+os.system("cat ChartData-1.html ChartData-2.html ChartData-3.html> ChartData.html")
 
-
-nodes = data['nodes']
-links = data['links']
-
-nodes_df = pd.DataFrame(nodes)
-links_df = pd.DataFrame(links)
-
-source_data = links_df.merge(nodes_df, how='left', left_on='source', right_index=True)
-source_data = source_data.merge(nodes_df, how='left', left_on='target', right_index=True)
-source_data = source_data[source_data["value"] > 5]
-
-chord_from_df = Chord(source_data, source="name_x", target="name_y", value="value")
-output_file('chord_from_df.html', mode="inline")
-show(chord_from_df)
+print("cat ChartData-1.html ChartData-2.html ChartData-3.html ChartData-4.html > ChartData.html")
+print("Use https://www.amcharts.com/demos/chord-diagram/ to generate chord diagram.")
+#############################################################
