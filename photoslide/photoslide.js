51edd74a27b207e4b67ac358b137cb8a1325c6a2
subhrendu1987 <subhrendu@idrbt.ac.in>
2023-06-14 15:54:02 +0530

    modified:   photoslide/gallery.html 	new file:   photoslide/gallery2.html 	modified:   photoslide/photoslide.css 	modified:   photoslide/photoslide.js

--- photoslide/photoslide.js
+++ photoslide/photoslide.js
@@ -1,24 +1,14 @@
     function openModal(imageSrc, caption) {
       var modal = document.getElementById("modal");
-      var modalImage = document.getElementById("modal-image");
+      var modalImg = document.getElementById("modal-img");
       var modalCaption = document.getElementById("modal-caption");
       
       modal.style.display = "block";
-      modalImage.src = imageSrc;
+      modalImg.src = imageSrc;
       modalCaption.innerText = caption;
     }
-    /********************************/
-   function closeModal() {
+    
+    function closeModal() {
       var modal = document.getElementById("modal");
-      
       modal.style.display = "none";
     }
-    /********************************/
-    window.onclick = function(event) {
-      var modal = document.getElementById("modal");
-      
-      if (event.target == modal) {
-        closeModal();
-      }
-    }
-
