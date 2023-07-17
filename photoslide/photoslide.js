616f8abca2828889ae31ca84830a2a10d70bb6ed
subhrendu1987 <subhrendu@idrbt.ac.in>
2023-07-17 08:31:39 +0530

    modified:   gallery.html 	modified:   mgallery.html 	new file:   photos/RBI-ED.webp 	modified:   photoslide.js

--- photoslide/photoslide.js
+++ photoslide/photoslide.js
@@ -1,14 +1,25 @@
-    function openModal(imageSrc, caption) {
-      var modal = document.getElementById("modal");
-      var modalImg = document.getElementById("modal-img");
-      var modalCaption = document.getElementById("modal-caption");
-      
-      modal.style.display = "block";
-      modalImg.src = imageSrc;
-      modalCaption.innerText = caption;
-    }
-    
-    function closeModal() {
-      var modal = document.getElementById("modal");
-      modal.style.display = "none";
-    }
+function findFigures() {
+  var figures = document.getElementsByTagName('figure');
+  for (var i = 0; i < figures.length; i++) {
+    var figure = figures[i];
+    var img = figure.querySelector('img');
+    var figcaption = figure.querySelector('figcaption');
+    figcaption.textContent = img.alt; 
+  }
+  alert("Figures ="+figures.length);
+}
+
+
+function openFrame(imgElement) {
+  var modal = document.getElementById("modal");
+  var modalImg = document.getElementById("modal-img");
+  var modalCaption = document.getElementById("modal-caption");
+  modal.style.display = "block";
+  modalImg.src =imgElement.src;
+  modalCaption.innerText = imgElement.alt;
+}
+
+function closeFrame() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
