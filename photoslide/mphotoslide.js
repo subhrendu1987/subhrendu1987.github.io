01f6fd4b392caff69e11086a94d9267d3a03b349
subhrendu1987 <subhrendu@idrbt.ac.in>
2023-07-17 13:48:30 +0530

    modified:   gallery.html 	modified:   mgallery.html 	modified:   mphotoslide.css 	new file:   mphotoslide.js 	modified:   photoslide.css

---
+++ photoslide/mphotoslide.js
@@ -0,0 +1,24 @@
+function figCaptionGen() {
+  var figures = document.getElementsByTagName('figure');
+  for (var i = 0; i < figures.length; i++) {
+    var figure = figures[i];
+    var img = figure.querySelector('img');
+    var figcaption = figure.querySelector('figcaption');
+    figcaption.textContent = img.alt; 
+  }
+}
+
+
+function openFrame(imgElement) {
+  var modal = document.getElementById("modal");
+  var modalImg = document.getElementById("modal-img");
+  var modalCaption = document.getElementById("modal-caption");
+  modal.style.display = "block";
+  modalImg.src =imgElement.src;
+  modalCaption.innerText = imgElement.alt;
+}
+
+function closeFrame() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
