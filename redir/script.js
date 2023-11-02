11a33120acc1105b741c8ebce6b8374d8f20a798
subhrendu1987 <subhrendu@idrbt.ac.in>
2023-11-02 21:37:54 +0530

    deleted:    PresentationURL.csv 	modified:   script.js

--- redir/script.js
+++ redir/script.js
@@ -1,13 +1,16 @@
+const webAppURL = 'https://script.googleusercontent.com/macros/echo?user_content_key=-nf7HtnAq_0htG1VeTn-eIqjG7DG2zmJVAB9hWC3ghXihLe0aPd4Z5uHfso8dyf3px8_s0aqadWfwTLmhpF3F8T4JLVykrupm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBgmdpr5BW5TgBjlEPPDofIM2_kNrBC71ajuEfM6JLC4DUaHCjcXJ17FzCh84gbx3gVxNwEBAPUaEcM9aEljYTZS6Ic6skGQNdz9Jw9Md8uu&lib=MiS8nA0IP1S5-p0rxnHc8P-ZNomVPhu3r';
 document.addEventListener('DOMContentLoaded', function() {
-    // URL of your web app that serves JSON data
-    const webAppURL = 'https://script.googleusercontent.com/macros/echo?user_content_key=-nf7HtnAq_0htG1VeTn-eIqjG7DG2zmJVAB9hWC3ghXihLe0aPd4Z5uHfso8dyf3px8_s0aqadWfwTLmhpF3F8T4JLVykrupm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBgmdpr5BW5TgBjlEPPDofIM2_kNrBC71ajuEfM6JLC4DUaHCjcXJ17FzCh84gbx3gVxNwEBAPUaEcM9aEljYTZS6Ic6skGQNdz9Jw9Md8uu&lib=MiS8nA0IP1S5-p0rxnHc8P-ZNomVPhu3r';
-
     // Reference to the table body
     const tableBody = document.querySelector('#data-table tbody');
 
     // Fetch JSON data from the web app
     fetch(webAppURL)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Unable to access JSON');
+            }
+            return response.json();
+        })
         .then(data => {
             // Loop through the JSON data and create table rows
             data.forEach(rowData => {
@@ -16,7 +19,16 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Loop through the object properties and create table cells
                 for (const key in rowData) {
                     const cell = document.createElement('td');
-                    cell.textContent = rowData[key];
+                    
+                    if (key === 'URL') { // Replace 'URLColumnName' with the actual column name
+                        const link = document.createElement('a');
+                        link.href = rowData[key];
+                        link.textContent = rowData[key];
+                        cell.appendChild(link);
+                    } else {
+                        cell.textContent = rowData[key];
+                    }
+                    
                     row.appendChild(cell);
                 }
 
@@ -24,6 +36,9 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         })
         .catch(error => {
-            console.error('Error fetching data:', error);
+            alert(error.message); // Display an alert with the error message
+        })
+        .finally(() => {
+            alert('Code execution is over.'); // Display an alert once code execution is complete
         });
 });
