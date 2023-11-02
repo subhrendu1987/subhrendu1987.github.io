5dc32a16895601620366fc8d092d2099e21f422e
subhrendu1987 <subhrendu@idrbt.ac.in>
2023-11-02 23:09:55 +0530

    modified:   script.js

--- redir/script.js
+++ redir/script.js
@@ -1,4 +1,5 @@
 const webAppURL = 'https://script.googleusercontent.com/macros/echo?user_content_key=-nf7HtnAq_0htG1VeTn-eIqjG7DG2zmJVAB9hWC3ghXihLe0aPd4Z5uHfso8dyf3px8_s0aqadWfwTLmhpF3F8T4JLVykrupm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBgmdpr5BW5TgBjlEPPDofIM2_kNrBC71ajuEfM6JLC4DUaHCjcXJ17FzCh84gbx3gVxNwEBAPUaEcM9aEljYTZS6Ic6skGQNdz9Jw9Md8uu&lib=MiS8nA0IP1S5-p0rxnHc8P-ZNomVPhu3r';
+
 document.addEventListener('DOMContentLoaded', function() {
     // Reference to the table body
     const tableBody = document.querySelector('#data-table tbody');
@@ -12,28 +13,27 @@ document.addEventListener('DOMContentLoaded', function() {
             return response.json();
         })
         .then(data => {
-            // Loop through the JSON data and create table rows
-            data.forEach(rowData => {
-                const row = document.createElement('tr');
+            for (const key in data) {
+                if (data.hasOwnProperty(key)) {
+                    const rowData = data[key];
+                    const row = document.createElement('tr');
 
-                // Loop through the object properties and create table cells
-                for (const key in rowData) {
-                    const cell = document.createElement('td');
-                    
-                    if (key === 'URL') { // Replace 'URLColumnName' with the actual column name
-                        const link = document.createElement('a');
-                        link.href = rowData[key];
-                        link.textContent = rowData[key];
-                        cell.appendChild(link);
-                    } else {
-                        cell.textContent = rowData[key];
+                    // Loop through the object properties and create table cells
+                    for (const cellKey in rowData) {
+                        const cell = document.createElement('td');
+                        if (cellKey === 'URL') {
+                            const link = document.createElement('a');
+                            link.href = rowData[cellKey];
+                            link.textContent = rowData[cellKey];
+                            cell.appendChild(link);
+                        } else {
+                            cell.textContent = rowData[cellKey];
+                        }
+                        row.appendChild(cell);
                     }
-                    
-                    row.appendChild(cell);
+                    tableBody.appendChild(row);
                 }
-
-                tableBody.appendChild(row);
-            });
+            }
         })
         .catch(error => {
             alert(error.message); // Display an alert with the error message
